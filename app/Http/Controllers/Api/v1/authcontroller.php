75ecb5d5180396c65b306d1804ec6eacee5175c3
Thomas Sarpong <sarpongthomas05@gmail.com>
2020-03-29 03:55:59 -0700

    set up outh2 to register users

---
+++ app/Http/Controllers/Api/v1/authcontroller.php
@@ -0,0 +1,50 @@
+<?php
+//use Laravel\Passport\HasApiTokens;
+namespace App\Http\Controllers\Api\v1;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class authcontroller extends Controller
+{
+    public function signUp(Request $request){
+        $user= new User();
+        $validated= $request->validate([
+            'name'=>'required|string',
+            'email' => 'email|required',
+            'password'=> 'required|confirmed',
+
+        ]);
+
+        if(!$validated){
+            return response(['message'=>'In']);
+        }
+        $password= bcrypt($request->password);
+        $user->name=$request->name;
+        $user->email = $request->email;
+        $user->password = $password;
+        $user->save();
+
+        $token = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'access_token' => $token]);
+    }
+    public function logIn(Request $request)
+    {
+
+        $validated = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+
+        ]);
+
+        if (!Auth::attempt($validated)) {
+            return response(['message' => 'Invalid login credentials']);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('authToken')->accessToken;
+        return response(['user'=> $user,'access_token'=>$token] );
+
+
+    }
+}
